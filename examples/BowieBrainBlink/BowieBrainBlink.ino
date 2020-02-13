8dd0498248204d76217f7f0eef06aa42524d3572
RobotGrrl <erin@robotgrrl.com>
2020-02-12 20:32:33 -0500

    Ading both leds to blink

--- examples/BowieBrainBlink/BowieBrainBlink.ino
+++ examples/BowieBrainBlink/BowieBrainBlink.ino
@@ -3,14 +3,18 @@
 // Run this first to make sure connection with the Teensy 3.6 works properly
 
 int led = 2;
+int board_led = 13;
 
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(board_led, OUTPUT);
 }
 
 void loop() {
   digitalWrite(led, HIGH);
+  digitalWrite(board_led, HIGH);
   delay(1000);
   digitalWrite(led, LOW);
+  digitalWrite(board_led, LOW);
   delay(1000);
 }
